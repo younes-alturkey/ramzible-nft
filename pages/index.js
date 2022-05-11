a87cd0051e56dbb498d05189edecd7ee6b10c389
Younes Alturkey <hi@younes.ninja>
2022-05-11 07:26:01 +0300

    optimize: nft qrcode soul

--- pages/index.js
+++ pages/index.js
@@ -8,39 +8,38 @@ export default function Home() {
     const [experience, setExperience] = useState(null)
     const [animation, setAnimation] = useState('idle')
     const [mode, setMode] = useState('default')
-    const cardRef = useRef(null)
     const nftImg = 'images/jeddah-season-banner.png'
     const qrImg = 'images/random-qr.png'
 
-    const changeImg = img => {
-        cardRef.current.style.backgroundImage = `url('${img}')`
-    }
-
     useEffect(() => {
-        changeImg(nftImg)
         setExperience(new Experience(document.querySelector('canvas.webgl')))
         console.log('rendered')
     }, [])
 
     return (
         <div className="h-screen w-screen flex justify-center items-center gap-4 relative">
-            <div
-                className="card relative bg-darkish p-3 z-10 bg-opacity-0"
-                ref={cardRef}
-            >
-                <div className="absolute bottom-4 right-4 text-white flex flex-row-reverse justify-center items-center gap-4">
+            <div className="relative p-3 select-none">
+                <img
+                    src={qrImg}
+                    className={`rounded-3xl ${mode !== 'qrCode' && 'hidden'}`}
+                />
+                <img
+                    src={nftImg}
+                    className={`rounded-3xl ${mode !== 'default' && 'hidden'}`}
+                />
+
+                <canvas
+                    className={`webgl z-10 ${mode !== 'soul' && 'hidden'}`}
+                ></canvas>
+                <div className="absolute bottom-8 right-8 text-white flex flex-row-reverse justify-center items-center gap-4">
                     <div
                         className={`p-3 rounded-full cursor-pointer bg-darkish hover:bg-opacity-70 transition-colors duration-200 ${
                             mode == 'qrCode' && ' text-red-500'
                         }`}
                         onClick={() => {
-                            if (mode !== 'qrCode') {
-                                setMode('qrCode')
-                                changeImg(qrImg)
-                            } else {
-                                setMode('default')
-                                changeImg(nftImg)
-                            }
+                            mode === 'qrCode'
+                                ? setMode('default')
+                                : setMode('qrCode')
                         }}
                     >
                         <ImQrcode size={24} />
@@ -50,20 +49,16 @@ export default function Home() {
                             mode == 'soul' && ' text-red-500'
                         }`}
                         onClick={() => {
-                            if (mode !== 'soul') {
-                                setMode('soul')
-                                changeImg('')
-                            } else {
-                                setMode('default')
-                                changeImg(nftImg)
-                            }
+                            mode === 'soul'
+                                ? setMode('default')
+                                : setMode('soul')
                         }}
                     >
                         <SiGhostery size={24} />
                     </div>
                 </div>
                 <div
-                    className={`absolute bottom-20 right-4 p-3 rounded-full cursor-pointer bg-darkish text-white hover:bg-opacity-70 transition-colors duration-200 ${
+                    className={`absolute bottom-24 right-8 p-3 rounded-full cursor-pointer bg-darkish text-white hover:bg-opacity-70 transition-colors duration-200 ${
                         mode !== 'soul' && 'hidden'
                     }`}
                     onClick={() => {
@@ -75,16 +70,15 @@ export default function Home() {
                 >
                     <GiHand size={28} />
                 </div>
-                <div className="absolute bottom-4 left-4 text-white flex flex-row-reverse justify-center items-center gap-4 select-none">
+                <div className="absolute bottom-8 left-8 text-white flex flex-row-reverse justify-center items-center gap-4 select-none">
                     <div className="bg-red-600 text-white p-3 rounded-full text-xl">
                         JED
                     </div>
                 </div>
-                <div className="absolute top-10 right-0 bg-red-500 text-white py-3 px-6 shadow text-xl select-none">
+                <div className="absolute top-10 right-3 bg-red-500 text-white py-3 px-6 shadow text-xl select-none">
                     <p>2022</p>
                 </div>
             </div>
-            <canvas className="webgl absolute z-0"></canvas>
         </div>
     )
 }
